609bc5f051007dfc4a2bd02e3768946ade6529a0
Siddhant Regmi <siddhantregmi121@gmail.com>
2024-12-24 11:36:06 +0545

    completed authentication and implemented authguard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, RolesModule, UsersModule],
+  imports: [PrismaModule, RolesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { RolesService } from 'src/roles/roles.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '15d' },
+    })
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, UsersService, RolesService],
+})
+export class AuthModule {}
